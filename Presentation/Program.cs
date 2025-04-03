6ef9aeac22e9afdb0b9dd785e7af22e3a41280e6
Unai Heguy--Tellechea <unai.heguy--tellechea@etu.estia.fr>
2025-04-03 19:48:24 +0200

    Created PollController and Index view

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using DataAccess.DataContext;
+using DataAccess.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<PollDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<PollRepository>();
+
 
 var app = builder.Build();
 
